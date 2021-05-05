c4fcc670c620acc6d62cf4ddac5bb82fee4e41ef
Veljko <veljko.cvetkovich@gmail.com>
2021-05-05 18:49:43 -0400

    l

--- FtcRobotController-master/FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalBot/BotIntake.java
+++ FtcRobotController-master/FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalBot/BotIntake.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
@@ -16,8 +17,10 @@ public class BotIntake {
     public DcMotor[] motors = new DcMotor[2];
     public CRServo finger;
     public boolean fingerOpen = false;
+    private ElapsedTime time = new ElapsedTime(0);//timer
 
     public BotIntake(DcMotor intakeLeft, DcMotor intakeRight, CRServo finger){
+
         motors[0] = intakeLeft;
         motors[1] = intakeRight;
         this.finger = finger;
@@ -27,21 +30,39 @@ public class BotIntake {
     }
 
     public startIntake(){
+
         motors[0].setPower(1);
         motors[1].setPower(-1);
     }
 
     public stopIntake(){
+
         motors[0].setPower(0);
         motors[1].setPower(0);
     }
 
     public void closeFinger(){
 
+        finger.setPower(1);//move towards intake
+
+        time.reset();
+
+        while(time.seconds() < 1);//waits 1 second for finger to close
+        finger.setPower(0);
+
+        fingerOpen = false;
     }
 
     public void openFinger(){
 
+        finger.setPower(-1);//move away from intake
+
+        time.reset();
+
+        while(time.seconds() < 1);//waits 1 second for finger to open
+        finger.setPower(0);
+
+        fingerOpen = true;
     }
 
 }
