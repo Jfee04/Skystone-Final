20d1024d12525cd1cb855dc2ec46ad2db77e5e87
Veljko <veljko.cvetkovich@gmail.com>
2021-05-05 19:40:53 -0400

    Updated

--- FtcRobotController-master/FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalBot/BotIntake.java
+++ FtcRobotController-master/FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalBot/BotIntake.java
@@ -1,16 +1,8 @@
 package org.firstinspires.ftc.teamcode.FinalBot;
 
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-
-import java.util.List;
 
 public class BotIntake {
 
@@ -29,13 +21,13 @@ public class BotIntake {
         motors[1].setPower(0);//right motor
     }
 
-    public startIntake(){
+    public void startIntake(){
 
         motors[0].setPower(1);
         motors[1].setPower(-1);
     }
 
-    public stopIntake(){
+    public void stopIntake(){
 
         motors[0].setPower(0);
         motors[1].setPower(0);
--- FtcRobotController-master/FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalBot/TeleOpIntake.java
+++ FtcRobotController-master/FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalBot/TeleOpIntake.java
@@ -1,4 +1,35 @@
 package org.firstinspires.ftc.teamcode.FinalBot;
 
-public class TeleOpIntake {
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+import java.util.List;
+
+@TeleOp(name="VeljkoTeleOp")
+public class TeleOpIntake extends OpMode {
+
+    BotIntake intake = new BotIntake(hardwareMap.get(DcMotor.class, "intakeLeft"), hardwareMap.get(DcMotor.class, "intakeRight"), hardwareMap.get(CRServo.class, "finger"));
+    
+    @Override
+    public void init() {
+        
+    }
+
+    @Override
+    public void loop() {
+
+        if(gamepad1.dpad_up){
+            intake.startIntake();
+        }
+        else if(gamepad1.dpad_down){
+            intake.stopIntake();
+        }
+    }
+
 }
